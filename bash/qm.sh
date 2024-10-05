3ba613974e14f2274947bb35d383c8015be1e71e
suiding007 <61475324+Suiding007@users.noreply.github.com>
2024-10-06 01:25:11 +0200

    Add files via upload

---
+++ bash/qm.sh
@@ -0,0 +1,5 @@
+qm clone 1000 201 --name espocrm1 -full -storage vm-pool
+qm set 201 --ipconfig0 ip=10.24.27.32/24,gw=10.24.27.1
+qm set 201 --nameserver 1.1.1.1
+qm set 201 --core 2 --memory 2048 --balloon 0
+qm start 201
